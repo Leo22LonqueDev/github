f1e7506aeeb4f921e1e49e4f61e6df263c4e5954
devlonque <leonardoarrudalonque@outlook.com>
2023-04-07 17:29:10 -0300

    Nova home finalizada

--- home.js
+++ home.js
@@ -1,3 +1,9 @@
 function nome(nome) {
     return nome
 }
+
+function sobreNome(sobreNome) {
+    return sobreNome
+}
+
+//Finalizado sistema da home
--- login.js
+++ login.js
@@ -1,3 +1,11 @@
 //Trabalhando no login
 
 //Finalizando o sistema de Login
+
+function login(nome, email) {
+    return 'Bem vindo';
+}
+
+
+// Sistema de login completo
+//Retornamos o nome do usuario logado
